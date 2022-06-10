52e76d62da5806d139858d70c5b8df7a20616f5d
anasbouabid <bouabidanas02@gmail.com>
2022-06-10 02:17:34 +0100

    members section on index in done (exepct P and VPs)

--- nice/event/views.py
+++ nice/event/views.py
@@ -5,73 +5,47 @@ from .models import *
 
 def index(request):
     events = Event.objects.all()
-    events_obj={"events":[]}
+    events_obj = {"events": []}
     # event
     for event in events:
-        event_info={}
-        event_info['event']=event
-        
-        event_info['eventlinks']=event.eventlink_set.all()
-        
-        event_info['eventlocalisation']=event.eventlocalisation
+        event_info = {}
+        event_info['event'] = event
+
+        event_info['eventlinks'] = event.eventlink_set.all()
+
+        event_info['eventlocalisation'] = event.eventlocalisation
         # event_info['speakers']=event.speaker_set.all()
-        
+
         events_obj['events'].append(event_info)
-      
 
     # sponsors
     sponsors = Sponsor.objects.all()
-    # members only chefs for the moments
-    chefs_obj =[]
-    members=Member.objects.all()
-    chefs_obj = members
-    
-        # if member.cell_set.filter(role="chef") or member.cell_set.filter(role="p") or member.cell_set.filter(role="vp"):
-        #     print(member)
-        #     chefs_obj.append(member)
-    # speakers
 
     speakers = Speaker.objects.all()
 
-
-
-    context ={
-        'events' : events_obj['events'] , 
-        'sponsors' : sponsors ,
-        'members' :chefs_obj,
+    context = {
+        'events': events_obj['events'],
+        'sponsors': sponsors,
+        'cells': Cell.objects.all(),
         'speakers': speakers
-        
     }
 
-    
-    return render(request,'index.html',context)
+    return render(request, 'index.html', context)
 
-def getEvent(request,id):
+
+def getEvent(request, id):
     event = Event.objects.get(pk=id)
     eventlinks = EventLink.objects.filter(event=event)
-    eventlocalisation=EventLocalisation.objects.filter(event=event)
+    eventlocalisation = EventLocalisation.objects.filter(event=event)
 
-    context = {'event':event,"eventlinks":eventlinks,"eventlocalisation":eventlocalisation}
+    context = {'event': event, "eventlinks": eventlinks,
+               "eventlocalisation": eventlocalisation}
 
-
-
-    return render(request,'event.html',context)
-
+    return render(request, 'event.html', context)
 
 
-    
 def getMembers(request):
+    cells = Cell.objects.all()
+    context = {"cells": cells}
 
-
-    cells= Cell.objects.all()
-
-    for cell in cells:
-        print("cell->",cell)
-        print("chef-->",cell.chef)
-        print("co chef---->",cell.co_chef)
-
-        for member in cell.members.all():
-            print('------->',member)
-        context = {"cells": cells}
-        
-    return render(request,'members.html', context)        
+    return render(request, 'members.html', context)
